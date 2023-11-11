1e9126e04e05f0c0c4eabaa7edc2af1762daf275
sübhan akbenli <suakbenli@gmail.com>
2023-11-11 03:12:40 +0300

    +++

---
+++ textsplit.py
@@ -0,0 +1,14 @@
+aaa="""Atv Turu
+Balon Turu 
+Araç Kiralama
+Scooter Kiralama
+Türk Gecesi
+Fotoğraf Çekimi
+Kiralik Elbise
+Jeep Safari
+At Turu 
+Derviş Gösterimi
+Yeşil Tur 
+Kırmızı Tur
+"""
+print(aaa.split("\n"))
